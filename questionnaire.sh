42d43578ce6dda050eba987afeab4240fd9b124a
Rukayat Adesoye <ruqqy.adesoye@gmail.com>
2024-08-29 13:04:56 +0000

    update program

--- questionnaire.sh
+++ questionnaire.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo ~~ Questionnaire ~~
+echo -e "\n~~ Questionnaire ~~\n"
 QUESTION1="What's your name?"
 echo $QUESTION1
 
@@ -11,3 +11,8 @@ echo $QUESTION2
 read LOCATION
 
 echo Hello $NAME from $LOCATION.
+
+QUESTION3="What's your favorite coding website?"
+echo $QUESTION3
+read WEBSITE
+echo -e "\nHello $NAME from $LOCATION. I learned that your favorite coding website is $WEBSITE!"
