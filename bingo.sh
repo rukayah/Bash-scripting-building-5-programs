6a6be61a5965e62dfb77ff9f9763bbc0a0d5a624
Rukayat Adesoye <ruqqy.adesoye@gmail.com>
2024-09-05 11:46:57 +0000

    add operation

--- bingo.sh
+++ bingo.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-#Bingo Number Generator
+# Bingo Number Generator
+
+echo -e "\n~~ Bingo Number Generator ~~\n"
+
+NUMBER=$(( RANDOM % 75 + 1 ))
+TEXT="The next number is, "
+
+echo $NUMBER
